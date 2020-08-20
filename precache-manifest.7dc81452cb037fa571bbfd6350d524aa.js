ad580d72b5a74acbd7925f3d1e187f70afec3c54
srajput45 <43792808+srajput45@users.noreply.github.com>
2020-08-20 19:47:18 +0530

    Updates

---
+++ precache-manifest.7dc81452cb037fa571bbfd6350d524aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f539a0995c89b8e6c6984f26236937a3",
+    "url": "/lpudemo/index.html"
+  },
+  {
+    "revision": "37d8d5aa3c9e0993e231",
+    "url": "/lpudemo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f847722ccacc01dc4fce",
+    "url": "/lpudemo/static/js/2.b013bc16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lpudemo/static/js/2.b013bc16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d8d5aa3c9e0993e231",
+    "url": "/lpudemo/static/js/main.8cad3ee9.chunk.js"
+  },
+  {
+    "revision": "349995a866a46806b3cf",
+    "url": "/lpudemo/static/js/runtime-main.0db7c1f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lpudemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
